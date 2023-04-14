68cf0a42af307534ba5c21f6fe7e8273d1b39d8d
x <hello@world.com>
2023-04-14 16:56:10 +0800

    Deployed e4aeb4e with MkDocs version: 1.4.2

---
+++ javascripts/baidu-statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2115da09e3989a4f984eb1303a91966a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
