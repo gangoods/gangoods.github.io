1e2e461401f25c172682c50b8ca755f5c42db45d
x <hello@world.com>
2023-09-10 20:18:29 +0800

    Deployed 267616f with MkDocs version: 1.4.2

--- javascripts/baidu-statistics.js
+++ javascripts/baidu-statistics.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?edfca759a4eea4a0211e1e0f7f826954";
+  hm.src = "https://hm.baidu.com/hm.js?1a17e3721fc78eb4a59f84dd919c3031";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
